c0d5ef7fae7037f7c5d37695ffbc085fc694faef
cwcraw <cwcraw@gmail.com>
2020-05-12 10:13:15 +0900

    Working on getting Heroku to recognize the Heroku DB

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const { typeDefs, resolvers } = require("./server/schema.js");
 const { ApolloServer } = require("apollo-server");
+require("dotenv").config
 
 const server = new ApolloServer({ typeDefs, resolvers });
 const port = process.env.PORT || 4000;
