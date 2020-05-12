1bd8e3ea0a3bbbc6ccbac7ac24e44b50805ac0d7
cwcraw <cwcraw@gmail.com>
2020-05-12 10:02:27 +0900

    Working on getting Heroku to recognize the Heroku DB

--- model/knexfile.js
+++ model/knexfile.js
@@ -5,8 +5,8 @@ require("dotenv").config();
 
 module.exports = {
   client: "pg",
-  // connection:  process.env.DATABASE_URL || config.db.connection,
-  connection:  process.env.DATABASE_URL || `postgres://${process.env.USER}:${process.env.PASSWORD}@127.0.0.1:5432/char_list`,
+  connection:  process.env.DATABASE_URL || config.db.connection,
+  // connection:  process.env.DATABASE_URL || `postgres://${process.env.USER}:${process.env.PASSWORD}@127.0.0.1:5432/char_list`,
     pool: {
     min: 2,
     max: 10
